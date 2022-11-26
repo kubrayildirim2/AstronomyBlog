dc652e2aaec315b4d3ad2f06e580c6089d351518
Kubra Yildirim <kbayrakci2@gmail.com>
2022-11-26 14:56:34 +0100

    Sign Up page

--- src/main/java/com/kubrabayrakci/AstronomyBlog/controller/AuthController.java
+++ src/main/java/com/kubrabayrakci/AstronomyBlog/controller/AuthController.java
@@ -1,13 +1,39 @@
 package com.kubrabayrakci.AstronomyBlog.controller;
 
+import com.kubrabayrakci.AstronomyBlog.model.User;
+import com.kubrabayrakci.AstronomyBlog.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
 
     @GetMapping("/login")
     public String getLoginPage(){
         return "login";
     }
+    @GetMapping("/register")
+    public String getRegisterPage(Model model){
+
+        User user = new User();
+        model.addAttribute("newUser", user);
+
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(Model model, @ModelAttribute("newUser") User newUser){
+
+        userDetailsService.saveUser(newUser);
+
+        return "redirect:/login";
+    }
+
 }
--- src/main/java/com/kubrabayrakci/AstronomyBlog/service/UserDetailsServiceImpl.java
+++ src/main/java/com/kubrabayrakci/AstronomyBlog/service/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kubrabayrakci.AstronomyBlog.service;
 
 import com.kubrabayrakci.AstronomyBlog.config.MyUserDetails;
+import com.kubrabayrakci.AstronomyBlog.model.Role;
 import com.kubrabayrakci.AstronomyBlog.model.User;
 import com.kubrabayrakci.AstronomyBlog.repository.RoleRepository;
 import com.kubrabayrakci.AstronomyBlog.repository.UserRepository;
@@ -10,9 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
 
-
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
@@ -20,6 +25,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     RoleRepository roleRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
@@ -35,5 +43,24 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         }
 
         return new MyUserDetails(user);
+    }
+    public void saveUser(User userToSave){
+
+        userToSave.setEnabled(true);
+
+        String encodedPassword = bCryptPasswordEncoder.encode(userToSave.getPassword());
+
+        userToSave.setPassword(encodedPassword);
+
+        Role role = roleRepository.findByName("USER");
+
+        Set<Role> roleSet = new HashSet<>();
+
+        roleSet.add(role);
+
+        userToSave.setRoles(roleSet);
+
+        userRepository.save(userToSave);
+
     }
 }
